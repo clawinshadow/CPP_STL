32d879ae6c8c7ed9e5fe7243dde95592cbad5af3
clawinshadow <fhuang@zongmutechh.com>
2020-02-14 17:24:53 +0800

    C++11 new features 2

--- cpp_11/new_features.cpp
+++ cpp_11/new_features.cpp
@@ -46,7 +46,76 @@ auto i = 42; //i has type int
 auto l = [] (int x) -> bool { return false; }; // l has type lambda
 #endif
 
+// uniform initialization & initializer list
+/* Before C++11, Initialization could happen with parentheses, braces, and/or assignment operators, 
+   For this reason, C++11 introduced the concept of uniform initialization, which means that for
+   any initialization, you can use one common syntax. This syntax uses braces
+*/ 
+
+struct P
+{
+public:
+    P(int, int) {}
+    explicit P(int, int, int) {}
+};
+
+void uniform_initialization_demo()
+{
+    // OK in each C++ version
+    int i;   //undefined value
+    int j(1);
+    int k = 5;
+    int arr[] = {1, 2, 3, 4, 5};
+    std::cout << "i: " << i << std::endl; //compile error, because i was not initialized
+
+#ifdef USE_CPP_11
+    // ok since C++11, braces means initializer list
+    int i2{}; 
+    int j2{1};
+    int k2 = {5};
+    int arr2[]{1, 2, 3, 4, 5};
+    std::cout << "i2 = " << i2 << std::endl; // initializer list forces value-initialization, initialize to zero or nullptr, instead of undefined value.
+#endif
+
+    // but initializer list does not support narrowing initialization, like double -> int
+    int x1(5.3);
+    int x2 = 5.3;
+#ifdef USE_CPP_11
+    // int x3{5.3};    //compile error of narrowing initialization
+    // int x4 = {5.3}; //compile error
+#endif    
+
+    // initializer list make "explicit" now also becomes relevant for constructors taking more than one argument
+    P p1(1, 2);                  //OK -> call P(int, int)
+    P p2{1, 2};                  //OK -> call P(int, int)
+    P p3{1, 2, 3};               //OK -> call P(int, int int) without implicit type conversion
+    P p4 = {4, 5};               //OK -> call P(int, int) with implicit type conversion allowed
+    // P p5 = {4, 5, 6};            //ERROR due to explicit (implicit type conversion forbidden)
+}
+
+// Range-based for loops
+void for_demo()
+{
+    for (int i: {1, 2, 3, 4, 5})
+    {
+        std::cout << i << std::endl;
+    }
+
+    // reference
+    std::vector<double> vec{1.0, 2.1, 3.3}; 
+    for (auto &elem: vec)
+    {
+        elem *= 2;
+    }
+    std::cout << "vec[1] = " << vec[1] << std::endl;
+
+    // const reference, avoid copy constructor & destructor during iteration
+    // for (const auto &elem: collection)..
+}
+
 void Run()
 {
-    nullptr_demo();
+    //nullptr_demo();
+    //uniform_initialization_demo();
+    for_demo();
 }
